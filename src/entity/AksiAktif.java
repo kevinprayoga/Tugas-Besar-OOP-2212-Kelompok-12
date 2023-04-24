9e627abb5361bf4f2d90edadddf47951d44dda9a
Nicholas <69570302+Nicholas438@users.noreply.github.com>
2023-04-25 00:03:46 +0700

    1

---
+++ src/entity/AksiAktif.java
@@ -0,0 +1,35 @@
+package entity;
+
+public interface AksiAktif {
+    public void kerja(int time);
+
+    public void olahraga(int time);
+
+    public void tidur(int time);
+
+    public void makan(Makanan m);
+
+    public void berkunjung(int time, Rumah r);
+
+    public void buangAir();
+
+    public int getTimeSTidur();
+
+    public int getTimeSBAir();
+
+    public int getSimCD();
+
+    public int getBonusInc();
+
+    public void vacation();
+
+    public void woodworking(NonMakanan item);
+
+    public void bath();
+
+    public void meditate(int time);
+
+    public void read();
+
+    public void party();
+}
---
+++ src/entity/Sims.java
@@ -0,0 +1,75 @@
+
+package entity;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Sims implements AksiAktif{
+    private int mood, money,kesehatan,kekenyangan, gajiBank;
+    private LocalDateTime endTime,startTime;
+    private boolean isBusy;
+    private String activity;
+
+    
+    
+    
+    
+    // Implementasi aksi aktif
+    public void kerja(int time) throws Exception{
+        if(mood-(time/30*10) <= 0){
+            throw new Exception("Sim tidak cukup mood untuk bekerja selama itu!");
+        } else if(kekenyangan - (time/30*10) <= 0){
+            throw new Exception("Sim tidak cukup kenyang untuk bekerja selama itu!");
+        } else{
+        startTime = LocalDateTime.now();
+        endTime = startTime.plusSeconds(time);
+        isBusy = true;
+        activity = "kerja";
+        }
+    }
+
+    public void update(){
+        if (activity.equals("kerja")){
+            if(LocalDateTime.now().compareTo(endTime) >= 0){
+                isBusy = false;
+                activity = "";
+            } else{
+                int seconds = (int) ChronoUnit.SECONDS.between(LocalDateTime.now(), startTime); 
+                mood -= (seconds/30*10);
+                kekenyangan -= (seconds/30*10);
+                gajiBank += seconds - (seconds%30);
+                money += ((gajiBank / 240)*(pekerjaan.getGaji()))*((100+getBonusInc())/100);
+                gajiBank = gajiBank % 240;
+                startTime.plusSeconds(seconds - (seconds%30));
+            }
+        } else if (activity.equals("olahraga")){
+            if(LocalDateTime.now().compareTo(endTime) >= 0){
+                isBusy = false;
+                activity = "";
+            } else{
+                int seconds = (int) ChronoUnit.SECONDS.between(LocalDateTime.now(), startTime); 
+                mood += (seconds/20*10);
+                kekenyangan -= (seconds/20*5);
+                kesehatan += (seconds/20*5);
+                startTime.plusSeconds(seconds - (seconds%20));
+            }
+        }
+    }
+
+    public void olahraga(int time) throws Exception {
+        kesehatan += (time / 20 * 5);
+        kekenyangan -= (time / 20 * 5);
+        mood += (time / 20 * 10);
+        if(kekenyangan - (time/20*10) <= 0){
+            throw new Exception("Sim tidak cukup kenyang untuk berolahraga selama itu!");
+        } else{
+            startTime = LocalDateTime.now();
+            endTime = startTime.plusSeconds(time);
+            isBusy = true;
+            activity = "olahraga";
+        }
+    }
+
+}
+
+
