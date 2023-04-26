6342576e1d4220cc1286c432cb38ea56a1d53eb5
ojan <16521411@mahasiswa.itb.ac.id>
2023-04-26 16:40:40 +0700

    Add game color palette

---
+++ src/graphics/ColorPalette.java
@@ -0,0 +1,10 @@
+package graphics;
+
+import java.awt.*;
+
+public class ColorPalette {
+    public static Color white = new Color(255, 255, 255);
+    public static Color light_grey = Color.decode("#E2E2E2");
+    public static Color dark_grey = Color.decode("#595959");
+
+}
