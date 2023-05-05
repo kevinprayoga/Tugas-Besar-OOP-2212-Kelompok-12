7dd56c8054f1c4bb72331a94a83eafb7528088bc
ojan <16521411@mahasiswa.itb.ac.id>
2023-05-05 10:10:51 +0700

    Actiob button fix

--- src/graphics/ActionButton.java
+++ src/graphics/ActionButton.java
@@ -8,11 +8,8 @@ import java.awt.event.MouseEvent;
 import javax.swing.JLabel;
 
 import entity.Sim;
-<<<<<<< HEAD
 import exceptions.NotEnoughKesejahteraan;
 import graphics.UI;
-=======
->>>>>>> af3b664472e68f08f780451bf0fb1e60c2e0401c
 import main.GamePanel;
 import util.UtilityTool;
 
@@ -76,19 +73,7 @@ public class ActionButton {
         eatLabel.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-<<<<<<< HEAD
                 UI.setActionText("kerja");
-=======
-                try{
-                    sim.kerja(120);
-                    for(Sim s:gamePanel.getPlayableSims()){
-                        s.update(120);
-                    }
-                } catch(Exception ex){
-                    System.out.println(ex.getMessage());
-                    
-                }
->>>>>>> af3b664472e68f08f780451bf0fb1e60c2e0401c
             }
         });
 
