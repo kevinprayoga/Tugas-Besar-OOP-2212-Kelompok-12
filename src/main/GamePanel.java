823c353d430d2ceea5d3b8fcd594f8e7f0b90072
ojan <16521411@mahasiswa.itb.ac.id>
2023-05-06 21:10:55 +0700

    Fixed dead sim

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -124,19 +124,23 @@ public class GamePanel extends JPanel implements Runnable {
 
     public synchronized void update() {
         if (Waktu.getActionTimer() == 0) {
+            ArrayList<Sim> dead = new ArrayList<>();
             for (Sim sims : playableSims) {
                 if (sims.getStatus() == "dead") {
-                    world.getSimList().remove(sims);
-                    for (int j = 0; j < 64; j++) {
-                        for (int k = 0; k < 64; k++) {
-                            if (world.getPerumahan().get(j, k) != null) {
-                                world.getPerumahan().get(j, k).removeSim(sims);
-                            }
-                        }
-                    }
-                    playableSims.remove(sims);
+                    dead.add(sims);
                     isSomeoneDied = true;
                 }
+            }
+            for (Sim sims : dead) {
+                world.getSimList().remove(sims);
+                for (int j = 0; j < 64; j++) {
+                    for (int k = 0; k < 64; k++) {
+                        if (world.getPerumahan().get(j, k) != null) {
+                            world.getPerumahan().get(j, k).removeSim(sims);
+                        }
+                    }
+                }
+                playableSims.remove(sims);
             }
             if (isSomeoneDied) {
                 if (playableSims.size() == 0) {
